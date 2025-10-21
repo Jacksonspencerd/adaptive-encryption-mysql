ef230c565fa5acccb8c5f8b095d542edbd8166eb
Jackson Davis <jacksonspencer.d@gmail.com>
2025-10-21 11:41:07 -0700

    Refactor threat_check.py comment for clarity and accuracy

--- util/threat_check.py
+++ util/threat_check.py
@@ -1,4 +1,5 @@
-# employs wsm method to evaluate threat level of a user
+# evaluates threat level based on input parameters
 # returns a threat score of 0-100
 #   higher score indicates higher threat level
 #   four threat levels: low (0-25), medium (26-50), high (51-75), critical (76-100)
+
